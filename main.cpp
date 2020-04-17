d4dc1b148e3830a9bdd97ab29cdabf27e3577968
Gleb (YurchenkovGA) <yurchencow@yandex.ru>
2020-04-17 12:31:26 +0300

    code: поиск ошибки

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ void show_histogram_text(const vector<double>& bins){
             cout<<" ";
         cout<<bin<<"|";
 
-            for(int i=0; i<bin; i++)
+            for(size_t i=0; i<bin; i++)
             {
                 cout<<"*";
             }
@@ -54,18 +54,42 @@ void show_histogram_text(const vector<double>& bins){
 
 
 }}
+
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void svg_text(double left, double baseline, string text) {
+     cout << "<text x='20' y='35'>anything you want</text>";}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    svg_begin(400, 300);
+    svg_text(20, 20, to_string(bins[0]));
+    svg_end();
+}
+
 int main()
 {
     size_t number_count;
-    cout<< "enter number count";
+    cerr<< "enter number count";
     cin>>number_count;
     const auto numbers=input_numbers(number_count);
 
     size_t bin_count;
-    cout<< "enter bin count";
+    cerr<< "enter bin count";
     cin>>bin_count;
     const auto bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
     return 0;
 
 }
