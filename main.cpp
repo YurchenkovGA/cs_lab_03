c023169125752446e4e9d0c9d248d1a45020b1fc
Gleb (YurchenkovGA) <yurchencow@yandex.ru>
2020-04-17 13:31:15 +0300

    code: marks.svg гистограмма

--- main.cpp
+++ main.cpp
@@ -23,10 +23,10 @@ void find_minmax(const vector<double>& numbers, double& min, double& max) {
             max=numbers[i];
         }}}
 
-vector<double> make_histogram(const vector<double>& numbers, size_t bin_count){
+vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count){
     double min, max;
     find_minmax(numbers, min, max);
-    vector<double>count(bin_count,0);
+    vector<size_t>count(bin_count,0);
     for (double x: numbers){
         size_t bin_index=(size_t)(x-min)*(bin_count)/(max-min);
         if (bin_index==bin_count)
@@ -70,11 +70,27 @@ void svg_end() {
 }
 
 void svg_text(double left, double baseline, string text) {
-     cout << "<text x='20' y='35'>anything you want</text>";}
+     cout << "<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";}
+
+void svg_rect(double x, double y, double width, double height,string stroke= "black", string fill= "black"){
+    cout<< "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";}
 
 void show_histogram_svg(const vector<size_t>& bins) {
-    svg_begin(400, 300);
-    svg_text(20, 20, to_string(bins[0]));
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins) {
+    const double bin_width = BLOCK_WIDTH * bin;
+    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    top += BIN_HEIGHT;
+}
     svg_end();
 }
 
